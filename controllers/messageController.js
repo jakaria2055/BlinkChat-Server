a30798539b9e9659a292886ca212c62cdce54b48
Jakaria Ahmed <jakaria88520@gmail.com>
2025-10-03 00:17:57 +0600

    get active user, get message, socket.io function, send message using socket.io

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,4 +1,6 @@
-import MessageModel from "../models/MessageModel";
+import MessageModel from "../models/MessageModel.js";
+import { io, userSocketMap } from "../server.js";
+import cloudinary from "../utility/cloudinary.js";
 
 //GET ALL USER
 export const getUsersForSidebar = async (req, res) => {
@@ -42,10 +44,56 @@ export const getMessages = async (req, res) => {
         { senderId: selectedUserId, receiverId: myId },
       ],
     });
-    await MessageModel.updateMany({senderId: selectedUserId, receiverId: myId}, {seen: true});
+    await MessageModel.updateMany(
+      { senderId: selectedUserId, receiverId: myId },
+      { seen: true }
+    );
 
-    req.json({success: true, messages})
+    req.json({ success: true, messages });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
+//MARK MESSAGE AS SEEN USING MESSAGE ID
+export const markMessageAsSeen = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await MessageModel.findByIdAndUpdate(id, { seen: true });
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//SEND MESSAGE
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const receiverId = req.params.id;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+    const newMessage = await MessageModel.create({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    //EMIT NEW MESSAGE TO RECEIVER SOCKET
+    const receiverSocketId =  userSocketMap[receiverId];
+    if(receiverSocketId){
+        io.to(receiverSocketId).emit("newMessage", newMessage)
+    }
+
+    res.json({success: true, newMessage})
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
--- server.js
+++ server.js
@@ -4,15 +4,43 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./config/database.js";
 import userRouter from "./api/api.js";
+import messageRouter from "./api/messageAPI.js";
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
+
+//IMPLENT SOCKET.io SERVER
+export const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+//STORE ONLINE USER
+export const userSocketMap = {};
+
+//Socket.io CONNECTION HANDLER
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("User Connected", userId);
+
+  if (userId) userSocketMap[userId] = socket.id;
+
+  //EMIT ONLINE USER TO ALL CONNECTED CLIENTS
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnected", () => {
+    console.log("User Disconnected", userId);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
 app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 app.use("/api/status", (req, res) => res.send("Server is Running fine..."));
 app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
 await connectDB();
 
