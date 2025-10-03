46d6128a601ad14a3af6d5bc95bc5f1be0111bea
Jakaria Ahmed <jakaria88520@gmail.com>
2025-10-03 22:45:26 +0600

    error resolve

--- server.js
+++ server.js
@@ -44,6 +44,11 @@ app.use("/api/messages", messageRouter);
 
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log("Server is running on PORT: ", +PORT));
+}
 
-server.listen(PORT, () => console.log("Server is running on PORT: ", +PORT));
+
+//EXPORT SERVER FOR VERCEL
+export default server;
